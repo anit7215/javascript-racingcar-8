e5e3341003beb13a11dac8675f588846ad6f5976
anit7215 <tina112703@gmail.com>
2025-10-26 02:26:48 +0900

    feat(App): 시도 횟수만큼 자동차 전진 또는 정지 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Console } from "@woowacourse/mission-utils";
+import { MissionUtils, Console } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
@@ -8,14 +8,31 @@ class App {
     const numberInput = await Console.readLineAsync(
       "시도할 횟수는 몇 회인가요?\n"
     );
-    this.createCars(carInput);
+    const cars = this.createCars(carInput);
+    const tryCount = Number(numberInput);
+
+    this.simulateRace(cars, tryCount);
   }
+
   createCars(carInput) {
     const cars = carInput.split(",").map((name) => ({
       name: name.trim(),
       position: 0,
     }));
     return cars;
+  }
+
+  simulateRace(cars, tryCount) {
+    for (let i = 0; i < tryCount; i++) {
+      this.racingRound(cars);
+    }
+  }
+
+  racingRound(cars) {
+    cars.forEach((car) => {
+      const randomNumber = MissionUtils.Random.pickNumberInRange(0, 9);
+      if (randomNumber >= 4) car.position += 1;
+    });
   }
 }
 
