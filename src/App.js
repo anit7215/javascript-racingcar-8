de37e99fd1ec6b0e7896b502391f543ba3039f97
anit7215 <tina112703@gmail.com>
2025-10-27 01:51:50 +0900

    feat(App): 자동차 이름 입력값 예외 처리

--- src/App.js
+++ src/App.js
@@ -5,16 +5,40 @@ class App {
     const carInput = await Console.readLineAsync(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
     );
+    this.validateCarNames(carInput);
+
     const numberInput = await Console.readLineAsync(
       "시도할 횟수는 몇 회인가요?\n"
     );
     const cars = this.createCars(carInput);
     const tryCount = Number(numberInput);
-    
-    Console.print("\n실행 결과");
 
+    Console.print("\n실행 결과");
     this.simulateRace(cars, tryCount);
     this.printWinners(cars);
+  }
+
+  validateCarNames(carInput) {
+    const names = carInput.split(",").map((name) => name.trim());
+
+    if (names.some((name) => name === "")) {
+      throw new Error("[ERROR] 자동차 이름은 공백일 수 없습니다.");
+    }
+
+    if (names.some((name) => name.length > 5)) {
+      throw new Error("[ERROR] 자동차 이름은 5자 이하만 가능합니다.");
+    }
+
+    if (new Set(names).size !== names.length) {
+      throw new Error("[ERROR] 자동차 이름은 중복될 수 없습니다.");
+    }
+    
+    const validNameRegex = /^[a-zA-Z0-9]+$/;
+    if (names.some((name) => !validNameRegex.test(name))) {
+      throw new Error(
+        "[ERROR] 자동차 이름은 공백이나 특수문자를 포함할 수 없습니다."
+      );
+    }
   }
 
   createCars(carInput) {
